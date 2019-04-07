ae81304bc5b3c027d7d90746b017f7297d114347
Akashneel Maiti <amai0002@student.monash.edu>
2019-04-07 21:12:51 +1000

    Major Edit v1

---
+++ VicGenesis/Controllers/VictoriasController.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VicGenesis.Models;
+
+namespace VicGenesis.Controllers
+{
+    public class VictoriasController : Controller
+    {
+        private regionalDatabaseEntities db = new regionalDatabaseEntities();
+        public static string Style { get; set; }
+        // GET: Victorias
+        public ActionResult Index()
+        {
+            return View(db.Victorias.ToList());
+        }
+
+        // GET: Victorias/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Victoria victoria = db.Victorias.Find(id);
+            if (victoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(victoria);
+        }
+
+        // GET: Victorias/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Victorias/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,COMM_CODE,Community_Name,Community_Type,DHS_Region,Map_Reference,Area_km_sq,Location,Road_distance_to_GPO_km,Local_Government_Area,Population_density,Primary_Care_Partnership,Number_of_localities,ABS_Remoteness_Area,Population_Difference,Funded_Activity_Types,Funded_Services,HACC_Services,Child_Protection_Services,Community_Health_Services,Homelessness,Disability_Services,Mental_Health_Services,Human_Health_Services_Sites,Human_Health_Services_Types,Dental_Sites,General_Practice_Sites,Pharmacy_Sites,Allied_Health_Sites,Complementary_Health_Sites,Child_Care_Sites,Residential_Aged_Care_Facilities,Licensed_Aged_Care_Places,Primary_Schools,Secondary_School,P12_schools,Other_Schools,Nearest_Health_Service,Distance_to_Nearest_Health_Service,Residential,Business,Industrial,Rural,Other,Number_Of_Households,Persons_Per_Household,Population_In_nonprivate_Dwellings,Public_Rental_Dwellings,Aboriginal_Or_Torres_Strait_Islander,Not_Completed_Year_12,Unemployed_Persons,Household_Income_Less_Than_650,Individual_Income_Less_Than_400,Household_Without_Motor_Vehicle,Household_With_Internet,One_Parent_Headed_Families,Persons_aged_75_Above_Living_Alone,Need_Assistance_With_Core_Activities,Born_Overseas,Non_Engisdh_Speaker_At_Home,Poor_English_Proficiency,Main_Cultural_group_1_Name,Main_cultural_group_1_Percentage,Main_cultural_group_2_Name,Main_cultural_group_2_Percentage,Main_cultural_group_3_Name,Main_cultural_group_3_Percentage")] Victoria victoria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Victorias.Add(victoria);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(victoria);
+        }
+
+        // GET: Victorias/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Victoria victoria = db.Victorias.Find(id);
+            if (victoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(victoria);
+        }
+
+        // POST: Victorias/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,COMM_CODE,Community_Name,Community_Type,DHS_Region,Map_Reference,Area_km_sq,Location,Road_distance_to_GPO_km,Local_Government_Area,Population_density,Primary_Care_Partnership,Number_of_localities,ABS_Remoteness_Area,Population_Difference,Funded_Activity_Types,Funded_Services,HACC_Services,Child_Protection_Services,Community_Health_Services,Homelessness,Disability_Services,Mental_Health_Services,Human_Health_Services_Sites,Human_Health_Services_Types,Dental_Sites,General_Practice_Sites,Pharmacy_Sites,Allied_Health_Sites,Complementary_Health_Sites,Child_Care_Sites,Residential_Aged_Care_Facilities,Licensed_Aged_Care_Places,Primary_Schools,Secondary_School,P12_schools,Other_Schools,Nearest_Health_Service,Distance_to_Nearest_Health_Service,Residential,Business,Industrial,Rural,Other,Number_Of_Households,Persons_Per_Household,Population_In_nonprivate_Dwellings,Public_Rental_Dwellings,Aboriginal_Or_Torres_Strait_Islander,Not_Completed_Year_12,Unemployed_Persons,Household_Income_Less_Than_650,Individual_Income_Less_Than_400,Household_Without_Motor_Vehicle,Household_With_Internet,One_Parent_Headed_Families,Persons_aged_75_Above_Living_Alone,Need_Assistance_With_Core_Activities,Born_Overseas,Non_Engisdh_Speaker_At_Home,Poor_English_Proficiency,Main_Cultural_group_1_Name,Main_cultural_group_1_Percentage,Main_cultural_group_2_Name,Main_cultural_group_2_Percentage,Main_cultural_group_3_Name,Main_cultural_group_3_Percentage")] Victoria victoria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(victoria).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(victoria);
+        }
+
+        // GET: Victorias/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Victoria victoria = db.Victorias.Find(id);
+            if (victoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(victoria);
+        }
+
+        // POST: Victorias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Victoria victoria = db.Victorias.Find(id);
+            db.Victorias.Remove(victoria);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        [HttpGet]
+        public ActionResult SelectGroup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SelectGroup(Victoria model)
+        {
+            Style = model.Style;
+            return RedirectToAction("Showgroup");
+        }
+
+        public ActionResult Showgroup()
+        {
+            return View(db.Victorias.ToList());
+        }
+    }
+}
--- VicGenesis/regionalDatabase.Context.cs
+++ VicGenesis/regionalDatabase.Context.cs
@@ -23,30 +23,155 @@ namespace VicGenesis
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Area>()
-                .Property(e => e.C_Area_Name_)
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.COMM_CODE)
                 .IsUnicode(false);
 
-            modelBuilder.Entity<Area>()
-                .Property(e => e.Description)
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Community_Name)
                 .IsUnicode(false);
 
-            modelBuilder.Entity<Area>()
-                .Property(e => e.Latitude)
-                .HasPrecision(10, 8);
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Community_Type)
+                .IsUnicode(false);
 
-            modelBuilder.Entity<Area>()
-                .Property(e => e.Longitude)
-                .HasPrecision(11, 8);
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.DHS_Region)
+                .IsUnicode(false);
 
-            modelBuilder.Entity<Event>()
-                .Property(e => e.Area_Name)
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Map_Reference)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Location)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Local_Government_Area)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Primary_Care_Partnership)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.ABS_Remoteness_Area)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Population_Difference)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Nearest_Health_Service)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Residential)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Business)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Industrial)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Rural)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Other)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Population_In_nonprivate_Dwellings)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Public_Rental_Dwellings)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Aboriginal_Or_Torres_Strait_Islander)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Not_Completed_Year_12)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Unemployed_Persons)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Household_Income_Less_Than_650)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Individual_Income_Less_Than_400)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Household_Without_Motor_Vehicle)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Household_With_Internet)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.One_Parent_Headed_Families)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Persons_aged_75_Above_Living_Alone)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Need_Assistance_With_Core_Activities)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Born_Overseas)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Non_Engisdh_Speaker_At_Home)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Poor_English_Proficiency)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Main_Cultural_group_1_Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Main_cultural_group_1_Percentage)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Main_cultural_group_2_Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Main_cultural_group_2_Percentage)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Main_cultural_group_3_Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Victoria>()
+                .Property(e => e.Main_cultural_group_3_Percentage)
                 .IsUnicode(false);
         }
     
         public virtual DbSet<Victoria> Victorias { get; set; }
-        public virtual DbSet<Announcement> Announcements { get; set; }
-        public virtual DbSet<Area> Areas { get; set; }
-        public virtual DbSet<Event> Events { get; set; }
     }
 }
